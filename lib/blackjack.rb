cb6b664de3680527ff86c889e0661dc890aa3043
Aleksandra Kosc <aleksandra.kosc@gmail.com>
2019-06-14 16:13:24 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -46,23 +46,12 @@ def hit?(num)
     puts card_total
   else 
     invalid_command
+      return hit?(num)
+
   end
-  return hit?(num)
   # code hit? here
 end
 
-def hit?(card_total)
-  prompt_user
-  user_input = get_user_input
-  if user_input == "h"
-    card_total += deal_card
-  elsif user_input == "s"
-    card_total
-  else
-    invalid_command
-    hit?(card_total)
-  end
-end
 
 
 def invalid_command
