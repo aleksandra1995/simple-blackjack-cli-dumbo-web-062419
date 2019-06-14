bd8bca80fb7b6e1c72981c3ad5930b501c24428f
Aleksandra Kosc <aleksandra.kosc@gmail.com>
2019-06-14 16:26:05 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -64,8 +64,11 @@ end
 
 def runner
   welcome
-  number = 21
-  until number > 0 
-  
+  card_total = initial_round
+  until card_total > 21
+    card_total = hit?(card_total)
+    display_card_total(card_total)
+  end
+  end_game(card_total)
 end
     
